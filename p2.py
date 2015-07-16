7e3c827b7a6dc602a08879243863b6119f120556
shagit <shashibang@gmail.com>
2015-07-16 11:08:50 -0700

    office project version deploying

--- p2.py
+++ p2.py
@@ -42,15 +42,15 @@ def index():
 		table = table.div(table.document["Total"], axis='index')
 		table = 100*np.round(table, 4)
 		table = table.fillna('')	
-	dir = 'C:\\Users\\TE236863\\Desktop\\App\\CSSC_Dashboard\\templates\\my_file.html'
+	dir = '/CSSC_Dashboard/templates/my_file.html'
 	if os.path.isfile(dir):
-		os.remove('C:\\Users\\TE236863\\Desktop\\App\\CSSC_Dashboard\\templates\\my_file.html')
+		os.remove('/CSSC_Dashboard/templates/my_file.html')
 		
-		Data = table.to_html(open('C:\\Users\\TE236863\\Desktop\\App\\CSSC_Dashboard\\templates\\my_file.html', 'w'))
+		Data = table.to_html(open('/CSSC_Dashboard/templates/my_file.html', 'w'))
 	else:
 		
-		Data = table.to_html(open('C:\\Users\\TE236863\\Desktop\\App\\CSSC_Dashboard\\templates\\my_file.html', 'w'))
-	os.chdir("C:\\Users\\TE236863\\Desktop\\App\\CSSC_Dashboard\\templates")
+		Data = table.to_html(open('/CSSC_Dashboard/templates/my_file.html', 'w'))
+	os.chdir("/CSSC_Dashboard/templates")
 	
 	with open("ph1.html") as index:
 		index_text = index.read()
